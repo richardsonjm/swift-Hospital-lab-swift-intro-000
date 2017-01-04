f90f16be6be8ac162f98244844df15c263be9116
richardsonjm <richardsonjm@gmail.com>
2017-01-04 16:28:35 -0500

    Schooling

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -2,14 +2,29 @@
 //  SchoolEmployees.swift
 //  Hospital
 
+struct AdvisoryBoardMember {
+    var name: String
+    var vacationDays = 30
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+extension AdvisoryBoardMember: Employee {}
 
+struct Principal {
+    var name: String
+    var vacationDays = 20
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
 
+extension Principal: Employee, Reprimand {}
 
+struct Teacher {
+    var name: String
+    var vacationDays = 15
+}
 
-
-
-
-
-
-
+extension Teacher: Employee, Teach {}
